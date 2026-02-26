1922e1d758d45a427ecb260540724308d9d88cca
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-27 01:04:12 +0600

    moved to python because of difficulties

--- py-test/main.py
+++ py-test/main.py
@@ -1,5 +1,31 @@
+class Solution:
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nmap = {}
+        for i, num in enumerate(nums):
+            if target - num in nmap:
+                return [nmap[target - num], i]
+            nmap[num] = i
+            print(nmap)
+    
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nmap = {}
+        for i, num in enumerate(nums):
+            if num in nmap:
+                return True
+            nmap[num] = i
+        return False
+    
+    
+        
+    
+
 def main():
-    print("Hello from py-test!")
+    print("Hello from py-test!\n")
+    x = Solution().containsDuplicate([1,1])
+    print(x)
+   
+    
 
 
 if __name__ == "__main__":
